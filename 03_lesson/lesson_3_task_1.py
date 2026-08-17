bbf84292205d87cf909b2f3130c1b4e5573b62a4
gimazitdinova-elvira <gimazitdinova.elvira@yandex.ru>
2026-08-17 11:33:06 +0500

    коммит

--- 03_lesson/lesson_3_task_1.py
+++ 03_lesson/lesson_3_task_1.py
@@ -2,6 +2,6 @@ from user import User
 
 my_user = User("Эльвира", "Гимазитдинова")
 
-print(my_user.get_first_name)
-print(my_user.get_last_name)
+print(my_user.get_first_name())
+print(my_user.get_last_name())
 print(my_user.get_full_info())
