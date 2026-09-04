162e2828a3bc20614eb97644d764e1ffc1e25174
gimazitdinova-elvira <gimazitdinova.elvira@yandex.ru>
2026-09-04 16:48:58 +0500

    коммит 6контрольный

---
+++ 06k_lesson/test_02_calc.py
@@ -0,0 +1,31 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+
+@pytest.fixture
+def browser():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    yield driver
+    driver.quit()
+
+
+def test_calculator(browser):
+    url = "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
+    browser.get(url)
+    delay_input = browser.find_element(By.CSS_SELECTOR, "#delay")
+    delay_input.clear()
+    delay_input.send_keys("45")
+
+    buttons = ["7", "+", "8", "="]
+    for button in buttons:
+        xpath = f"//span[text()='{button}']"
+        browser.find_element(By.XPATH, xpath).click()
+
+    assert WebDriverWait(browser, 46).until(
+        EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".screen"), "15")
+    )
